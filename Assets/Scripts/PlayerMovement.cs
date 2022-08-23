ec2f98dde2a14357f3a0be77fe41eb7228a37fb0
Mat <mat555333@gmail.com>
2022-08-23 13:56:30 +0200

    PlayerMovement to Input System

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : NetworkBehaviour
@@ -17,15 +18,24 @@ public class PlayerMovement : NetworkBehaviour
     Vector3 playerVelocity;
     Vector3 moveDir;
 
+    InputActionAsset actions;
+
     float gravityValue = -9.81f;
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        actions = GetComponent<PlayerInput>().actions;
     }
 
     public override void OnNetworkSpawn()
     {
         if (!IsOwner) Destroy(this);
+    }
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        if (!controller.isGrounded) return;
+        playerVelocity.y += jumpHeight;
     }
 
     void Update()
@@ -34,20 +44,12 @@ public class PlayerMovement : NetworkBehaviour
         {
             playerVelocity.y = 0f;
         }
+        Vector2 input = actions.FindAction("Move").ReadValue<Vector2>();
+        moveDir = transform.forward * input.y + transform.right * input.x;
 
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
-
-        moveDir = transform.forward * verticalInput + transform.right * horizontalInput;
         controller.Move(moveDir.normalized * walkSpeed * Time.deltaTime);
 
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            if (!controller.isGrounded) return;
-            playerVelocity.y += Mathf.Sqrt(jumpHeight);
-        }
     }
 }

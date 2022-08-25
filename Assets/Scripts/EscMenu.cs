27389eb444a764fa42d4934b8871ee8ff80c3bbc
Mat <mat555333@gmail.com>
2022-08-25 08:55:55 +0200

    Toggle EscMenu

--- Assets/Scripts/EscMenu.cs
+++ Assets/Scripts/EscMenu.cs
@@ -5,9 +5,15 @@ using Unity.Netcode;
 
 public class EscMenu : MonoBehaviour
 {
+    [SerializeField] GameObject menuPanel;
     public void Disconnect()
     {
         NetworkManager.Singleton.Shutdown();
         UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ToggleMenu()
+    {
+        menuPanel.SetActive(!menuPanel.activeSelf);
     }
 }

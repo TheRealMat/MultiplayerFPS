83ffaac1816e4d32ef39f0e6e31a554c73dfe917
Mat <mat555333@gmail.com>
2022-08-31 08:20:09 +0200

    Rigidbody Projectiles

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,26 +6,18 @@ using Unity.Netcode;
 public class Projectile : NetworkBehaviour
 {
     [SerializeField] int damage = 30;
-    [SerializeField] float dropoff = 0;
-    [SerializeField] float projectilespeed = 10;
+    [SerializeField] float launchSpeed = 500;
     [SerializeField] float projectileLifetime = 5;
 
     [HideInInspector] public ulong projectileOwner;
 
     // do SphereCastAll for explosions
 
-    Vector3 projectileVelocity;
-
     private void Start()
     {
         if (!IsServer) return;
+        GetComponent<Rigidbody>().AddForce(transform.forward * launchSpeed);
         Destroy(gameObject, projectileLifetime);
-    }
-    private void Update()
-    {
-        transform.position += transform.forward * projectilespeed * Time.deltaTime;
-        projectileVelocity.y += dropoff * Time.deltaTime;
-        transform.position += Vector3.down * projectileVelocity.y * Time.deltaTime;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -36,7 +28,7 @@ public class Projectile : NetworkBehaviour
         {
             if (projectileOwner == other.gameObject.GetComponent<NetworkObject>().OwnerClientId)
             {
-                // use the third overload to turn off the ignorecollision after half a second or so, maybe check OnTriggerExit
+                // should use the third overload to turn off the ignorecollision after half a second or so, maybe check OnTriggerExit
                 Physics.IgnoreCollision(other, GetComponent<Collider>());
                 return;
             }
@@ -44,7 +36,7 @@ public class Projectile : NetworkBehaviour
         }
         else if (other.gameObject.tag == "Projectile")
         {
-            // perhaps some projectiles should be able to collide
+            // perhaps some projectiles should be able to collide?
             if(other.GetComponent<Projectile>().projectileOwner == this.projectileOwner)
             {
                 return;
